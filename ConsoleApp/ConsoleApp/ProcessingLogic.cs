26bebc85d861f97f44b3af3be4f08f7f767e2692
nkundu <nicholas.kundu@gmail.com>
2016-04-09 23:48:49 -0400

    initial

---
+++ ConsoleApp/ConsoleApp/ProcessingLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi.Core.Interfaces;
+
+namespace ConsoleApp
+{
+    public class TweetProcessor
+    {
+        private ITweet m_tweet;
+        public TweetProcessor(ITweet tweet)
+        {
+            m_tweet = tweet;
+        }
+
+        public void Process()
+        {
+            if (!Eligible())
+                return;
+
+            Console.WriteLine(m_tweet.Text);
+        }
+
+        private bool Eligible()
+        {
+            return m_tweet.Language == Tweetinvi.Core.Enum.Language.English;
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Core.Credentials;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ini = new Ini.IniFile(@"C:\twitter.ini");
+
+            // Set up your credentials
+            Auth.SetCredentials(new TwitterCredentials
+            {
+                AccessToken = ini.IniReadValue("Config", "Access_Token"),
+                AccessTokenSecret = ini.IniReadValue("Config", "Access_Secret"),
+                ConsumerKey = ini.IniReadValue("Config", "Consumer_Key"),
+                ConsumerSecret = ini.IniReadValue("Config", "Consumer_Secret")
+            });
+
+            var stream = Tweetinvi.Stream.CreateSampleStream();
+            stream.TweetReceived += (sender, a) =>
+            {
+                var tp = new TweetProcessor(a.Tweet);
+
+                tp.Process();
+            };
+            stream.StartStream();
+        }
+    }
+}
